656f2d0ff7067f75ace86f623352cd5e65ed3eca
Andrew Schembri <Andrew.Schembri.c10273@mcast.edu.mt>
2020-10-23 15:23:42 +0200

    Commit

---
+++ Solution1/PresentationWebApp/Controllers/SupportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationWebApp.Controllers
+{
+	public class SupportController : Controller
+	{
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Contact(string email, string query)
+		{
+
+			ViewData["feedback"] = "Thank you for getting in touch with us!";
+
+			return View();
+		}
+	}
+}
