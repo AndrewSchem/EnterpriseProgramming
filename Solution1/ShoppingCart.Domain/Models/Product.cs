2c6d8d48cb9c97cc4b2eae55bc8b30a3e8991824
AndrewSchem <andrew.schembri.c10273@mcast.edu.mt>
2020-10-30 15:10:57 +0100

    Database

---
+++ Solution1/ShoppingCart.Domain/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ShoppingCart.Domain.Models
+{
+	public class Product
+	{	
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public Guid Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public double Price { get; set; }
+		[Required]
+		public string Description { get; set; }
+		[Required]
+		public Category Category { get; set; }
+	}
+}
