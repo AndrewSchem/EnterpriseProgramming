284dc58550cc243583c5ca220c2ca829358da3f2
AndrewSchem <andrew.schembri.c10273@mcast.edu.mt>
2020-11-13 13:40:43 +0100

    Commit

---
+++ Solution1/ShoppingCart.IOC/ShoppingCart.IOC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.Application.Interfaces;
+using ShoppingCart.Domain.Interfaces;
+using Stripe;
+using System;
+
+namespace ShoppingCart.IOC
+{
+	public class DependencyContainer
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			services.AddScoped<IProductsRepository, IProductsRepository>();
+			services.AddScoped<IProductsService, ProductService>();
+		}
+	}
+}
